17eb56c25e570e98bd3af4dc904813ef107b841d
MatiT <mat.tenorio@duocuc.cl>
2026-04-14 03:54:36 -0400

    third

--- src/main/java/com/Taskflow/EvaluacionParcialEA1/controller/TareaController.java
+++ src/main/java/com/Taskflow/EvaluacionParcialEA1/controller/TareaController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Taskflow.EvaluacionParcialEA1.model.Tarea;
 import com.Taskflow.EvaluacionParcialEA1.service.TareaService;
 
-@RequestMapping("api/tareas")
+@RequestMapping("/api/tareas")
+@RestController
 public class TareaController {
     @Autowired
     private TareaService service;
